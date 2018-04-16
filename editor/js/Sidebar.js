6c80f1a7b3eaf86b19705032b39020904d4bb24f
michael <mbradford@nextvr.com>
2018-04-16 16:49:47 -0700

    add Animations tab with ability to play loaded animatinos

--- editor/js/Sidebar.js
+++ editor/js/Sidebar.js
@@ -12,10 +12,11 @@ var Sidebar = function ( editor ) {
 	var sceneTab = new UI.Text( 'SCENE' ).onClick( onClick );
 	var projectTab = new UI.Text( 'PROJECT' ).onClick( onClick );
 	var settingsTab = new UI.Text( 'SETTINGS' ).onClick( onClick );
+	var animationsTab = new UI.Text( 'ANIMATIONS' ).onClick( onClick );
 
 	var tabs = new UI.Div();
 	tabs.setId( 'tabs' );
-	tabs.add( sceneTab, projectTab, settingsTab );
+	tabs.add( sceneTab, projectTab, settingsTab, animationsTab );
 	container.add( tabs );
 
 	function onClick( event ) {
@@ -45,6 +46,11 @@ var Sidebar = function ( editor ) {
 	);
 	container.add( settings );
 
+	var animations = new UI.Span().add(
+		new Sidebar.Animation( editor )
+	);
+	animations.setDisplay('none');
+	container.add( animations );
 	//
 
 	function select( section ) {
@@ -52,10 +58,12 @@ var Sidebar = function ( editor ) {
 		sceneTab.setClass( '' );
 		projectTab.setClass( '' );
 		settingsTab.setClass( '' );
+		animationsTab.setClass( '' );
 
 		scene.setDisplay( 'none' );
 		project.setDisplay( 'none' );
 		settings.setDisplay( 'none' );
+		animations.setDisplay( 'none' );
 
 		switch ( section ) {
 			case 'SCENE':
@@ -69,6 +77,10 @@ var Sidebar = function ( editor ) {
 			case 'SETTINGS':
 				settingsTab.setClass( 'selected' );
 				settings.setDisplay( '' );
+				break;
+			case 'ANIMATIONS':
+				animationsTab.setClass( 'selected' );
+				animations.setDisplay( '' );
 				break;
 		}
 
